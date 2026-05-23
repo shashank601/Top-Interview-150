34d012d720560c27d5ac8a1e04f7c675fa23faa4
shashank <shashank3fr@gmail.com>
2026-05-23 19:25:43 +0530

    Create 53. Maximum Subarray.cpp

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int ans = nums[0];                        <-- not 0
+
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];     
+
+          
+            ans = max(sum, ans);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return ans;
+        
+    }
+};
