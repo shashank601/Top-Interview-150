929a92d3427f4ddd73cc2dacd360136b0e3ef792
shashank <shashank3fr@gmail.com>
2026-05-25 01:00:47 +0530

    Clarify insertion order in hashmap with comment
    Added a comment to clarify the reason for the order of insertion in the hashmap.

---
+++ Hashmap/1. Two Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+
+        for (int i = 0; i < nums.size(); i++) {
+        
+            if (m.count(target - nums[i])) {
+                return {i, m[target - nums[i]]};
+            }
+            
+            m[nums[i]] = i;                        <-- adding later to avoid self duplicate
+        }
+
+        return {};
+    }
+};
