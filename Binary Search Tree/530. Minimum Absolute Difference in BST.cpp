988e95ccc4190bd31e5a05651119b0b44a59765f
shashank <shashank3fr@gmail.com>
2026-05-25 18:51:21 +0530

    Update 530. Minimum Absolute Difference in BST.cpp

--- Binary Search Tree/530. Minimum Absolute Difference in BST.cpp
+++ Binary Search Tree/530. Minimum Absolute Difference in BST.cpp
@@ -43,3 +43,41 @@ basically if i want to change a value of some older pushed stack frame withou un
 
 prev = root->val;
 inside deepest recursion updates the shared variable seen by all callers.
+
+
+=======
+
+dfs(root->right, root->val, ans);
+
+You are thinking:
+
+"I already passed current value, so why need reference?"
+Because only the RIGHT subtree gets that updated value.
+The parent stack frame itself does not change.
+
+
+
+Now return back upward.
+At parent node 3:
+its local prev is STILL -1.
+
+
+Why?
+
+Because node 1 modified only its own copy.
+So when parent processes:
+root->val - prev
+it becomes:
+
+3 - (-1) <-- wrong
+
+========
+
+the "previous node" is not local to a subtree.
+It is global traversal state.
+That is why reference is needed.
+
+
+so like imagine a tree 
+
+that has just left nodes  so right dfs() call return null no impact on prev so now root sees that right prev 
